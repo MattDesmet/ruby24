f7e52c2538a134619cf1936a32d84972a8ab1ae4
Matt Desmet <mattd.email@gmail.com>
2017-02-16 11:25:55 -0800

    added EDIT route to Porfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,4 +6,35 @@ class PortfoliosController < ApplicationController
   def new
     @portfolio_item = Portfolio.new
   end
+
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'The Portfolio ITEM was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image))
+        format.html {redirect_to portfolios_path, notice: 'Your entry has been updated successfully'}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
 end
